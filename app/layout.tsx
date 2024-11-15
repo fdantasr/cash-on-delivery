728f428f400928042950b5bfd67d132eb79d3ef2
Felipe Dantas <dantas.contatod@gmail.com>
2024-11-15 19:03:17 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,16 +4,6 @@ import { ThemeProvider } from 'next-themes';
 import Provider from '@/components/Provider';
 import './globals.css';
 
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : 'http://localhost:3000';
-
-export const metadata = {
-  metadataBase: new URL(defaultUrl),
-  title: 'Next.js and Supabase Starter Kit',
-  description: 'The fastest way to build apps with Next.js and Supabase',
-};
-
 export default function RootLayout({
   children,
 }: {
